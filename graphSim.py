957c8c5add86425eccf1e54070efac5ec8581128
aoomimkonnaiwaaa <61964403+aoomimkonnaiwaaa@users.noreply.github.com>
2022-05-06 10:44:46 +0700

    Update Graph

--- graphSim.py
+++ graphSim.py
@@ -1,5 +1,5 @@
 import pygame
-import sys
+#import sys
 from pygame.locals import *
 import pygame.math
 import math
@@ -11,23 +11,34 @@ pygame.init()
 DISPLAYSURF = pygame.display.set_mode((1280,720))
 pygame.display.set_caption('The Amazing Circus')
 done = False
-# bgImg = os.path.join('/Users/aoomimkonnaiwaaa/Simulation Project/Projectile-Simulation/background-01.png')
-# background = pygame.image.load(bgImg)
-# DISPLAYSURF.blit(background, (1280,720))
-#bgImg = pygame.image.load('/Users/aoomimkonnaiwaaa/Simulation Project/Projectile-Simulation/background-01.png')
-DISPLAYSURF.blit(bgImg, (1280,720))
+#background
+bgImg = pygame.image.load('background-01.png')
+DISPLAYSURF.blit(bgImg,(0,0))
+#shooter gun
+bgImg = pygame.image.load('shooterGraph.png')
+DISPLAYSURF.blit(bgImg,(50,400))
+# Set the size for the image
+# DEFAULT_IMAGE_SIZE = (165.5, 163.5)
+# Scale the image to your needed size
+# bgImg = pygame.transform.scale(bgImg, DEFAULT_IMAGE_SIZE)
+#goal
+bgImg = pygame.image.load('goal.png')
+DISPLAYSURF.blit(bgImg,(990,570))
+shooterIcon = pygame.image.load('shooter.ico')
+pygame.display.set_icon(shooterIcon)
+ 
 
 v0 = 5.536 
 h = 0.2
 alpha = 60
-timestep = 0.005
+timestep = 0.006
 gravity = -9.81
 init_accel = pygame.Vector2(0,gravity)
 
 
 #Calculation
 
-init_vel = pygame.Vector2(math.cos(alpha*math.pi/180)*v0,math.sin(alpha * math.pi/180)*v0)
+init_vel = pygame.Vector2(math.cos(alpha * math.pi/180)*v0,math.sin(alpha * math.pi/180)*v0)
 init_pos = pygame.Vector2(0,h)
 pos = pygame.Vector2()
 time = 0
@@ -41,7 +52,7 @@ while not done: # main game loop
     pos = init_pos + init_vel * time + init_accel * time * time * 0.5
     time += timestep
 
-    pygame.draw.circle(DISPLAYSURF,(0,255,125), [50+pos.x*280,500-pos.y*112.5],4.5)
+    pygame.draw.circle(DISPLAYSURF,(255,255,255), [230+pos.x*280,520-pos.y*100],4.5)
     pygame.display.update()
 
 pygame.quit()
