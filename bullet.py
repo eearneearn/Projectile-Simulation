e61ae1ee5f5d0b43594310f390d5d37b1d127a52
aoomimkonnaiwaaa <61964403+aoomimkonnaiwaaa@users.noreply.github.com>
2022-05-08 14:55:26 +0700

    graph update

---
+++ bullet.py
@@ -0,0 +1,87 @@
+from pygame.locals import *
+import pygame.math # for vector2
+import math
+# import main
+# import calculation
+
+g = -9.81 #down
+
+class bulletSim:
+    def __init__(self,initialVelo,angle,timestep,x,y,bullet,position):
+        self.initialVelo = 5.536 #initial velocity
+        self.y_axis = 0 #sy from start
+        self.angle = 60 #angle
+        self.timestep = 0.01
+        self.time = 0
+        self.initialAccel = pygame.Vector2(0,g)
+        self.initialVelo2 = pygame.Vector2(math.cos(angle * math.pi/180)*initialVelo,math.sin(angle * math.pi/180)*initialVelo)
+        self.initialPost = pygame.Vector2(0,y_axis) # x_axis,y_axis
+        self.position = pygame.Vector2()
+        self.bulletImg = pygame.bulletImg.load('bullet.png')
+        self.position = self.position
+    
+    def draw(self,surface):
+        pygame.draw.circle(DISPLAYSURF,(38,255,0),[250 + position.x * 280,500 - position.y * 150],4.5) # y ต้องเป็นลบ เพราะทิศลง
+        surface.blit(self.bulletImg, [self. position.x * 280 + 250 ,500 - position.y * 150])
+    
+    def update(self):
+        if self.position.y >= - 0.2 : 
+            self.position = self.initialPost + (self.initialVelo2 * self.time) + ((1/2) * self.initialAccel * self.time ** 2) # s = intialPostion + ut + (1/2at^2)
+            self.time = self.time + self.timestep
+            
+                        
+# pygame.init()
+# DISPLAYSURF = pygame.display.set_mode((1280,720))
+# pygame.display.set_caption('The Amazing Circus: Projectile Simulation')
+# done = False
+
+# #background
+# bgImg = pygame.image.load('background-01.png')
+# DISPLAYSURF.blit(bgImg,(0,0))
+
+# #shooter gun
+# bgImg = pygame.image.load('shooterGraph2.png')
+# DISPLAYSURF.blit(bgImg,(90,433))
+# # Set the size for the image
+# # DEFAULT_IMAGE_SIZE = (165.5, 163.5)
+# # Scale the image to your needed size
+# # bgImg = pygame.transform.scale(bgImg, DEFAULT_IMAGE_SIZE)
+
+# #goal
+# bgImg = pygame.image.load('goalNew.png')
+# DISPLAYSURF.blit(bgImg,(900,530))
+
+# #icon
+# shooterIcon = pygame.image.load('shooter.ico')
+# pygame.display.set_icon(shooterIcon)
+ 
+# v0 = 5.536 #initial velocity
+# h = 0 #sy from start
+# alpha = 60 #angle
+# timestep = 0.006
+# gravity = -9.81 #down
+# initialAccel = pygame.Vector2(0,gravity)
+
+# #Calculation
+
+# initialVelo2 = pygame.Vector2(math.cos(alpha * math.pi/180)*v0,math.sin(alpha * math.pi/180)*v0)
+# initialPost = pygame.Vector2(0,h) # x_axis,y_axis
+# position = pygame.Vector2()
+# time = 0
+
+# while not done: # main game loop
+#     for event in pygame.event.get():
+#         if  event.type == QUIT:
+#             done = True
+#             pressed = pygame.key.get_pressed()
+                       
+   
+#     if position.y >= - 0.2 : 
+#         position = initialPost + (initialVelo2 * time) + ((1/2) * initialAccel * time ** 2) # s = intialPostion + ut + (1/2at^2)
+#         time = time + timestep
+#         pygame.draw.circle(DISPLAYSURF,(38,255,0),[250 + position.x * 280,500 - position.y * 150],4.5) # y ต้องเป็นลบ เพราะทิศลง
+#     pygame.display.update()
+#     #print(pos.y)
+    
+
+# pygame.quit()
--- graphSim.py
+++ graphSim.py
@@ -1,47 +1,14 @@
 from pygame.locals import *
 import pygame.math # for vector2
 import math
-# import main
-# import calculation
+from bullet import bulletSim
 
 pygame.init()
-DISPLAYSURF = pygame.display.set_mode((1280,720))
+DISPLAYSURF = pygame.display.set_mode((1280,720)) #bg
 pygame.display.set_caption('The Amazing Circus: Projectile Simulation')
 done = False
-
-#background
-bgImg = pygame.image.load('background-01.png')
-DISPLAYSURF.blit(bgImg,(0,0))
-
-#shooter gun
-bgImg = pygame.image.load('shooterGraph2.png')
-DISPLAYSURF.blit(bgImg,(90,435))
-# Set the size for the image
-# DEFAULT_IMAGE_SIZE = (165.5, 163.5)
-# Scale the image to your needed size
-# bgImg = pygame.transform.scale(bgImg, DEFAULT_IMAGE_SIZE)
-
-#goal
-bgImg = pygame.image.load('goalNew.png')
-DISPLAYSURF.blit(bgImg,(900,500))
-
-#icon
-shooterIcon = pygame.image.load('shooter.ico')
-pygame.display.set_icon(shooterIcon)
- 
-v0 = 5.536 #initial velocity
-h = 0 #sy from start
-alpha = 60 #angle
-timestep = 0.006
-gravity = -9.81 #down
-init_accel = pygame.Vector2(0,gravity)
-
-#Calculation
-
-init_vel = pygame.Vector2(math.cos(alpha * math.pi/180)*v0,math.sin(alpha * math.pi/180)*v0)
-init_pos = pygame.Vector2(0,h) # x_axis,y_axis
-pos = pygame.Vector2()
-time = 0
+bulletShoot = bulletSim(position.x,position.y,initialVelo,angle,timestep)
+ิbulletShootList = [bulletShoot]
 
 while not done: # main game loop
     for event in pygame.event.get():
@@ -49,13 +16,9 @@ while not done: # main game loop
             done = True
             pressed = pygame.key.get_pressed()
                        
+    for i in range(len(bulletShoot)):
+        bulletShoot.update()
+        bulletShoot[i].draw(DISPLAYSURF)
+    pygame.display.update()   
    
-    if pos.y >= - 0.2 : # บวกเพิ่ม - 0.7 
-        pos = init_pos + (init_vel * time) + ((1/2) * init_accel * time ** 2) # s = intialPostion + ut + (1/2at^2)
-        time = time + timestep
-        pygame.draw.circle(DISPLAYSURF,(255,255,255),[230 + pos.x * 280,490 - pos.y * 100],4.5) # y ต้องเป็นลบ เพราะทิศลง
-    pygame.display.update()
-    print(pos.y)
-    
-
-pygame.quit()
+pygame.quit()  
